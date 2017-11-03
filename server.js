bfe6b0942ff223028676ecc4e701e09f5eb11c99
unknown <casca_de_fier@yahoo.com>
2017-11-03 08:30:00 +0200

    refactor to work with local host

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bP = require('body-parser');
 let nodemailer = require('nodemailer');
+let sanitizer = require('sanitizer');
 const keys = require('./config/keys');
 let app = express();
 const PORT = process.env.PORT || 8081;
@@ -8,23 +9,17 @@ const PORT = process.env.PORT || 8081;
 app.use(bP.urlencoded({extended: true}));
 app.use(bP.json());
 
-app.post('/send/email', (req, res) => {
-  var jsonString = '';
-  req.on('data', function (data) {
-    jsonString += data;
-});
-
-req.on('end', function () {
-    console.log(jsonString);
-    for(var item in jsonString){
-      console.log(item);
-    }
-})
-});
-
-app.get('/', (req, res) => {
-  sendEmail(res);
-  // res.render('index');
+app.post('/', (req, res) => {
+  let errors = checkEmail(req.body);
+  for(let it in req.body){
+    sanitizer.sanitize(req.body[it].toString())
+  };
+  if(Object.keys(errors).length == 0){
+    console.log("Sending email");
+    sendEmail(req.body, res);
+  }else{
+    console.log(errors);
+  }
 });
 
 app.listen(PORT, () => {
@@ -41,22 +36,52 @@ function sendEmail(mailData, res) {
   });
 
   var mailOptions = {
-    from: mailData.sender, // sender address
-    to: 'thedevcat9@mail.com', // list of receivers
-    subject: mailData.subject, // Subject line
-    text: mailData.text //, // plaintext body
-    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    from: mailData.contactEmail,
+    to: 'thedevcat@mail.com', // list of receivers
+    subject: mailData.contactSubject, // Subject line
+    // text: mailData.contactMessage //, // plaintext body
+    html: `<h3>Sender: ${mailData.contactName} - ${mailData.contactEmail}</h3><p>Subject: ${mailData.contactSubject}</p><p>Text: ${mailData.contactMessage}</p>` // You can choose to send an HTML body instead
   };
 
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
         console.log(error);
         res.json({yo: 'error'});
-    }else{
-        console.log('Message sent: ' + info.response);
-        res.json({yo: info.response});
-    };
+    }
 });
 
+}
+
+function checkEmail(data) {
+  let error = {};
+  if(data.contactName){
+    if(data.contactName.length >= 30){
+      error.contactName = "Length of Contact Name is to big."
+    }
+  }else{
+    error.contactName = "Contact Name field does not exist."
+  }
+  if(data.contactEmail){
+    if(data.contactEmail.indexOf('@') == -1 || data.contactEmail.indexOf('.') == -1){
+      error.contactEmail = "Contact Email is not of email type."
+    }
+  }else{
+    error.contactEmail = "Contact email field does not exist."
+  }
+  if(data.contactSubject){
+    if(data.contactSubject.length >= 30){
+      error.contactSubject = "Length of Contact Subject is to big."
+    }
+  }else{
+    error.contactSubject = "Contact Subject field does not exist."
+  }
+  if(data.contactMessage){
+    if(data.contactMessage.length >= 4000){
+      error.contactMessage = "Length of Message is to big."
+    }
+  }else{
+    error.contactMessage = "Message field does not exist."
+  }
+  return error;
 }
 
